8995026b535cf9a33147680c50656f5b2fa9817a
sunsheng <941216988@qq.com>
2026-03-11 10:08:53 +0800

    123

--- src/views/chongQing/system/version/components/detail.vue
+++ src/views/chongQing/system/version/components/detail.vue
@@ -3,37 +3,39 @@
   <el-dialog title="查看详情" v-model="open" width="600px" append-to-body :show-close="false" :close-on-click-modal="false" :draggable="true">
     <el-card>
       <table border width="100%" cellspacing="0" cellpadding="8" style="border-color: #e6f0f1">
-        <tr>
-          <td width="20%" style="color: #409eff">版本ID：</td>
-          <td style="color: #818489">{{ detailData.versionId }}</td>
-        </tr>
-        <tr>
-          <td style="color: #409eff">版本号：</td>
-          <td style="color: #818489">{{ detailData.versionNum }}</td>
-        </tr>
-        <tr>
-          <td style="color: #409eff">更新地址：</td>
-          <td style="color: #818489">{{ detailData.versionUrl }}</td>
-        </tr>
-        <tr>
-          <td style="color: #409eff">上传时间：</td>
-          <td style="color: #818489">{{ detailData.vcreateTime }}</td>
-        </tr>
-        <tr>
-          <td style="color: #409eff">上传人：</td>
-          <td style="color: #818489">{{ detailData.workName }}</td>
-        </tr>
-        <tr>
-          <td style="color: #409eff">状态：</td>
-          <td>
-            <el-tag type="danger" v-if="detailData.versionState == 2">{{ GetLabelByValue(VersionStatus, detailData.versionState) }}</el-tag>
-            <el-tag type="success" v-else>{{ GetLabelByValue(VersionStatus, detailData.versionState) }}</el-tag>
-          </td>
-        </tr>
-        <tr>
-          <td style="color: #409eff">备注：</td>
-          <td style="color: #818489">{{ detailData.versionRemark }}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td width="20%" style="color: #409eff">版本ID：</td>
+            <td style="color: #818489">{{ detailData.versionId }}</td>
+          </tr>
+          <tr>
+            <td style="color: #409eff">版本号：</td>
+            <td style="color: #818489">{{ detailData.versionNum }}</td>
+          </tr>
+          <tr>
+            <td style="color: #409eff">更新地址：</td>
+            <td style="color: #818489">{{ detailData.versionUrl }}</td>
+          </tr>
+          <tr>
+            <td style="color: #409eff">上传时间：</td>
+            <td style="color: #818489">{{ detailData.vcreateTime }}</td>
+          </tr>
+          <tr>
+            <td style="color: #409eff">上传人：</td>
+            <td style="color: #818489">{{ detailData.workName }}</td>
+          </tr>
+          <tr>
+            <td style="color: #409eff">状态：</td>
+            <td>
+              <el-tag type="danger" v-if="detailData.versionState == 2">{{ GetLabelByValue(VersionStatus, detailData.versionState) }}</el-tag>
+              <el-tag type="success" v-else>{{ GetLabelByValue(VersionStatus, detailData.versionState) }}</el-tag>
+            </td>
+          </tr>
+          <tr>
+            <td style="color: #409eff">备注：</td>
+            <td style="color: #818489">{{ detailData.versionRemark }}</td>
+          </tr>
+        </tbody>
       </table>
     </el-card>
     <template #footer>
